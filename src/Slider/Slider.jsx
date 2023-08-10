53f0665b0af6edfa59cfacc300f4544cfdc7cdf4
UserNaneless <vgripor@gmail.com>
2023-08-10 20:14:54 +0500

    Phone

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -29,17 +29,19 @@ export default class Slider extends Component {
     onMouseClick = (e) => {
         this.mouseDown = true;
         e.currentTarget.style.userSelect = "none";
-
     }
+
     onMouseUp = (e) => {
         if(this.mouseDown){
             this.mouseDown = false;
 
             let slowDown = setInterval(() => {
-                let forecast = this.sliderRef.current;
+                let forecast = e.target;
+
+
                 forecast.scrollLeft -= this.mouseLastMove;
                 if (this.mouseLastMove != 0) {
-                    this.mouseLastMove = this.mouseLastMove - Math.sign(this.mouseLastMove) * .25;
+                    this.mouseLastMove = this.mouseLastMove - Math.sign(this.mouseLastMove) * .2;
                 }
                 else if (this.mouseLastMove == 0 || forecast.scrollLeft + 1 > forecast.scrollWidth - forecast.offsetWidth) {
                     clearInterval(slowDown);
@@ -48,24 +50,12 @@ export default class Slider extends Component {
         }
     }
 
-    componentDidMount() {
-        this.sliderRef.current.addEventListener("wheel", this.onMouseScroll);
-        this.sliderRef.current.addEventListener("mousemove", this.onMouseMove);
-        this.sliderRef.current.addEventListener("mousedown", this.onMouseClick);
-        this.sliderRef.current.addEventListener("mouseup", this.onMouseUp);
-    }
-
-    componentWillUnmount() {
-        this.sliderRef.current.removeEventListener("wheel", this.onMouseScroll);
-        this.sliderRef.current.removeEventListener("mousemove", this.onMouseMove);
-        this.sliderRef.current.removeEventListener("mousedown", this.onMouseClick);
-        this.sliderRef.current.removeEventListener("mouseup", this.onMouseUp);
-
-    }
 
     render() {
         return (
-            <div ref={this.sliderRef} className={this.props.className + " slider"}>
+            <div onWheel={this.onMouseScroll} onMouseMove={this.onMouseMove} onMouseDown={this.onMouseClick} 
+                 onMouseUp={this.onMouseUp}
+            className={this.props.className + " slider"}>
                 {this.props.scrollItems}
             </div>
         )
